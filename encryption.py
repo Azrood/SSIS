0850aba73043e70df18137eb0173b690108f9d2f
Azrood <53349565+Azrood@users.noreply.github.com>
2021-06-01 19:41:37 +0100

    Salsa20 encryption/decryption functions + pseudo random generation + key extraction function from a secret string

---
+++ encryption.py
@@ -0,0 +1,33 @@
+import hashlib
+import random
+from typing import List
+import numpy as np
+from Crypto.Cipher import Salsa20
+from Crypto.Util.number import bytes_to_long
+
+def extract_key(key: str):
+    """Derive a key1 and key2 from a password str and returns a hex tuple (key1, key2) """
+    digest = hashlib.sha256(key).digest()
+    key1 = hex(bytes_to_long(digest))
+    key2 = hashlib.sha256(digest).hexdigest()
+
+    return key1, key2 
+
+def encrypt(msg: bytes, key: bytes) -> bytes:
+    """Encrypt a plaintext message with key using Salsa20"""
+    cipher = Salsa20.new(key=key)
+    encrypted_message = cipher.encrypt(msg)
+    return encrypted_message
+
+def decrypt(msg: bytes, key: bytes) -> bytes:
+    """Decrypt a plaintext message with key using Salsa20"""
+    cipher = Salsa20.new(key=key)
+    decrypted_message = cipher.decrypt(msg)
+    return decrypted_message
+
+def prng(message: bytes, seed: int, image: np.array) -> List[int]:
+    """Generates an array of random locations with pseudo-random sequence based on seed"""
+    random.seed(seed)
+    random_location_array = random.sample(range(0, image.size), len(message))
+    return random_location_array
+
