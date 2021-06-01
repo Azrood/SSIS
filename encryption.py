aac9e1d4f99a28a6dcfa7e1cc29addf2c1a8dc83
Azrood <53349565+Azrood@users.noreply.github.com>
2021-06-01 23:49:16 +0100

    modulation function

--- encryption.py
+++ encryption.py
@@ -25,9 +25,15 @@ def decrypt(msg: bytes, key: bytes) -> bytes:
     decrypted_message = cipher.decrypt(msg)
     return decrypted_message
 
-def prng(message: bytes, seed: int, image: np.array) -> List[int]:
+def prng(message: bytes, seed: int, image: np.ndarray) -> List[int]:
     """Generates an array of random locations with pseudo-random sequence based on seed"""
     random.seed(seed)
     random_location_array = random.sample(range(0, image.size), len(message))
     return random_location_array
 
+def modulation(pseudo_rand_array: List[int], message: bytes, img: np.ndarray) -> np.ndarray:
+    """Modulates a message into an image with a pseudo random sequence array """
+    lines, columns = img.shape
+    for i,char in zip(pseudo_rand_array, message):
+        img[i//columns][i%columns] = img[i//columns][i%columns]*char
+    return img
