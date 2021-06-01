a65552b35cec39425ad05ff496f5d8008c923256
Azrood <53349565+Azrood@users.noreply.github.com>
2021-06-01 19:41:53 +0100

    RGB planes extraction and orthogonalization functions

--- imeji.py
+++ imeji.py
@@ -1,10 +1,13 @@
-import numpy
+import numpy as np
 import PIL
 
 def extract_planes(img: PIL.Image.Image):
-    planes = numpy.array(img)
-    return *planes
+    """Return a n x m x 3 array, each n x m array is a plane R,G,B"""
+    planes = np.array(img)
+    R, G, B = planes[..., 0], planes[..., 1], planes[..., 2]
+    return R, G, B
 
-def orthogonalize():
-    numpy.qr
-    # TODO : gram-schmidt orthogonalisation
+def orthogonalize(matrix: np.array) -> np.array:
+    """Orthogonalizes a matrix into Q and R and return the orthogonal matrix Q"""
+    q, r = np.linalg.qr(matrix)
+    return q
