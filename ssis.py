459e4102c8e545de7f7940385cdd4e6aab585111
Azrood <53349565+Azrood@users.noreply.github.com>
2021-06-10 20:34:24 +0100

    extraction function

--- ssis.py
+++ ssis.py
@@ -1,6 +1,7 @@
 import argparse
 from base64 import b64decode, b64encode
 from binascii import hexlify, unhexlify
+from string import printable
 
 import numpy as np
 from Crypto.Util.number import bytes_to_long, long_to_bytes
@@ -29,27 +30,47 @@ extract.add_argument("-o", "--output", help="Le chemin du fichier image de l'ima
 
 args = parser.parse_args()
 
-def extract_image(src: str, passwd: str):
-    decryption_key, seed_prng_key, deinterleaver_seed_key = extract_keys(passwd)
-    img = oniichan(src)
+def extract_image(steg: str, passwd: str, out: str):
+    decryption_key, seed_prng_key, deinterleaver_seed_key = extract_keys(passwd.encode())
+    img = np.array(Image.open(steg))
+    stego_array = img[..., -1]
+    img_size = stego_array.size
     #steps: deinterleave, demodulate, decrypt
-    rand_seq_interleaver = prng()
-    deinterleaver()
+
+    deinterleaver_seed_key = bytes_to_long(unhexlify(deinterleaver_seed_key))
+    rand_seq_interleaver = prng(stego_array.size, deinterleaver_seed_key, img_size)
+    deinterleaved_img = deinterleaver(rand_seq_interleaver, stego_array)
+    img[..., -1] = deinterleaved_img
+
+    
+    seed_prng_key = bytes_to_long(unhexlify(seed_prng_key))
+    rnd_loc_array = prng(stego_array.size, seed_prng_key, img_size)
+    #demodulated_msg = demodulation(rnd_loc_array, img)
+    demodulated_msg = ''.join( x for x in demodulation(rnd_loc_array, img) if x in printable)
+    decryption_key = unhexlify(decryption_key)
+    
+    
+    demodulated_msg = b64decode(demodulated_msg)
+    msg = decrypt(demodulated_msg, decryption_key)
+    
+    imouto(msg, out)
+
+
 
 def embed_image(src: str, cover: str, passwd: str, output: str) -> None:
     encryption_key, seed_prng_key, interleaver_seed_key = extract_keys(passwd.encode())
-    source_img = oniichan(src).encode()
 
     cover_img = np.array(Image.open(cover))
     cover_img_size = cover_img.size // cover_img.shape[-1]
+    source_img = oniichan(src).encode()
     # encrypt, modulate, interleave
     encryption_key = unhexlify(encryption_key)
     encrypted_message = b64encode(encrypt(source_img, encryption_key))
 
     seed_prng_key = bytes_to_long(unhexlify(seed_prng_key))
-    rnd_loc_array = prng(len(encrypted_message), seed_prng_key, cover_img_size)
+    rnd_loc_array = prng(cover_img_size, seed_prng_key, cover_img_size)
     modulated = modulation(rnd_loc_array, encrypted_message.decode(), cover_img)
-
+    
     interleaver_seed_key = bytes_to_long(unhexlify(interleaver_seed_key))
     rnd_intrlvr_array = prng(modulated[..., -1].size, interleaver_seed_key, cover_img_size)
     interleaved = interleaver(rnd_intrlvr_array, modulated[..., -1])
@@ -65,8 +86,8 @@ if __name__ == "__main__":
         embed_image(args.source, args.cover, args.key, args.output)
 
         
-    elif args.extract:
-        pass
+    elif args.steg == "extract":
+        extract_image(args.stego, args.key, args.output)
 
     
     
